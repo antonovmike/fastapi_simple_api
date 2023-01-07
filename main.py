1937e2d3bd0c15618ce3296d4eb8b54acb21d535
antonovmike <antonvma@outlook.com>
2023-01-07 07:45:18 +0500

    Default value

--- main.py
+++ main.py
@@ -9,7 +9,18 @@ app = FastAPI()
 def root():
     return FileResponse("public/index.html")
  
- 
+ # Send form
+# @app.post("/postdata")
+# def postdata(username = Form(), userage=Form()):
+    # return {"name": username, "age": userage}
+
+# Send form + Validation
 @app.post("/postdata")
-def postdata(username = Form(), userage=Form()):
+def postdata(username: str = Form(min_length=2, max_length=9), userage: int = Form(ge=18, lt=111)):
     return {"name": username, "age": userage}
+
+# Default value
+@app.post("/postdata")
+def postdata(username: str = Form(default ="Undefined", min_length=2, max_length=9), userage: int = Form(default=18, ge=18, lt=111)):
+    return {"name": username, "age": userage}
+    
