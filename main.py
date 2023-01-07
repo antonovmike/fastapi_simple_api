1e86c2dcbc3300163379c7f8bb3852ec2dac07f1
antonovmike <antonvma@outlook.com>
2023-01-07 07:25:13 +0500

    Cookies as an Object

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from datetime import datetime
 
 app = FastAPI()
 
+# Cookies as a Parameter
+# @app.get("/")
+# def root(response: Response):
+    # now = datetime.now() # Get current Date and Time
+    # response.set_cookie(key="last_visit", value=now)
+    # return {"message": "Cookies are set"}
+
+
+# Cookies as an Object
 @app.get("/")
-def root(response: Response):
+def root():
     now = datetime.now() # Get current Date and Time
+    response = JSONResponse(content={"message": "Cookies are set"})
     response.set_cookie(key="last_visit", value=now)
-    return {"message": "cookie are set"}
+    return response
